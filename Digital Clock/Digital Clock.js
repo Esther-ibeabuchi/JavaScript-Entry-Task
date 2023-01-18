ae3d64630cd1c14807617a8fa38e2bd45b7e0060
Esther-ibeabuchi <114227224+Esther-ibeabuchi@users.noreply.github.com>
2023-01-18 19:14:44 +0100

    Add files via upload

---
+++ Digital Clock/Digital Clock.js
@@ -0,0 +1,58 @@
+const hoursEl = document.getElementById("hours")
+const minuteEl = document.getElementById("minutes")
+const secondEl = document.getElementById("seconds")
+const ampmEl = document.getElementById("ampm")
+
+// let updateClock = () => {
+//     let h = new Date().getHours()
+//     let m = new Date().getMinutes()
+//     let s = new Date().getSeconds()
+//     let ampm = 'AM'
+
+
+//     if (h > 12) {
+//         h = h -12
+//         ampm = "PM"
+//     }
+    
+//     h = h<10 ? "0" + h: h;
+//     m = m<10 ? "0" + m: m;
+//     s = s<10 ? "0" + s: s
+
+//     hoursEl.innerText = h;
+//     minuteEl.innerText = m;
+//     secondEl.innerText = s
+//     ampmEl.innerText = ampm
+
+//     setTimeout(()=> {
+//         updateClock()
+//     }, 1000)
+
+// }
+// updateClock()
+
+const updateClock = () => {
+let h = new Date().getHours()
+let m = new Date().getMinutes()
+let s = new Date().getSeconds()
+let ampm = "AM"
+
+if (h > 12) {
+    h = h - 12
+    ampm = "PM"
+}
+h = h < 10 ? "0" + h : h
+m = m < 10 ? "0" + m : m
+s = s < 10 ? "0" + s : s
+
+hoursEl.innerText = h
+minuteEl.innerText = m
+secondEl.innerText = s
+ampmEl.innerText = ampm
+
+setTimeout(()=> {
+    updateClock()
+}, 1000)
+}
+
+updateClock()
